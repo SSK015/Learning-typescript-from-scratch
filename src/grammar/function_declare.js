b3364283e3bec3ec79082db056024d7af9935994
SSK015 <18252677381@163.com>
2023-07-14 14:47:15 +0800

    basicgrammar

--- src/grammar/function_declare.js
+++ src/grammar/function_declare.js
@@ -3,3 +3,15 @@ var mySum = function (x, y) {
 };
 var answer = mySum(5, 6);
 console.log(answer);
+function push(array) {
+    var items = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        items[_i - 1] = arguments[_i];
+    }
+    items.forEach(function (item) {
+        array.push(item);
+    });
+}
+var a = [];
+push(a, 1, 2, 3);
+console.log(a);
--- src/grammar/function_declare.ts
+++ src/grammar/function_declare.ts
@@ -5,3 +5,13 @@ let mySum: (x: number, y: number) => number = function (x: number, y: number): n
 let answer = mySum(5,6);
 
 console.log(answer);
+
+function push(array, ...items) {
+    items.forEach(function(item) {
+        array.push(item);
+    });
+}
+
+let a: any[] = [];
+push(a, 1, 2, 3);
+console.log(a);
